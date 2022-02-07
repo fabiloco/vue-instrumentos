1b4e21f9e16861a7d70bf224a17fc91413c85eb8
allrivenjs <jaimeruizxdd2@gmail.com>
2022-02-07 16:09:10 -0500

    show items product beutifull

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -11,7 +11,7 @@
 
 			<div v-else class="products__list__container">
 				<aside class="list__aside">
-					<h3 class="list__aside__title">Compra por categoria</h3>
+					<h3 class="list__aside__title text-lg ">Compra por categoria</h3>
 					<div class="list__aside-box"></div>
 					<ul id="products-categories">
 						<li>
@@ -38,7 +38,7 @@
 						</li>
 					</ul>
 
-					<h3 class="list__aside__title">Tu lista de deseos</h3>
+					<h3 class="list__aside__title text-lg">Tu lista de deseos</h3>
 					<div class="list__aside-box"></div>
 					<ul>
 						<li>
@@ -47,105 +47,63 @@
 						</li>
 					</ul>
 				</aside>
-				<div class="list__products">
-					<div class="list__products__options">
-						<div class="list__products__options-item">
-							<div class="list__products__options-btn">
-								<img
-									class="list__products__options-btn-ico"
-									src="../assets/icons/view-grid.svg"
-								/>
-							</div>
-							<div class="list__products__options-btn">
-								<img
-									class="list__products__options-btn-ico"
-									src="../assets/icons/view-list.svg"
-								/>
-							</div>
-						</div>
-						<div class="list__products__options-item">
-							<div class="list__products__options-btn">
-								Mostrar 10 por página
-								<img
-									src="assets/icons/down-arrow.svg"
-									class="down-arrow"
-								/>
-							</div>
-						</div>
-						<div class="list__products__options-item">
-							<div class="list__products__options-btn">
-								Ordenar por precio
-								<img
-									src="assets/icons/down-arrow.svg"
-									class="down-arrow"
-								/>
-							</div>
-						</div>
-					</div>
 
-					<ul
-						id="products"
-						class="flex flex-wrap justify-center p-2 overflow-hidden"
-					>
-						<li
-							v-for="(product, index) in products"
-							v-bind:key="index"
-						>
-							<router-link
-								class="w-full h-full"
-								v-bind:to="`/product/${product.id}`"
-							>
-								<img
-									v-bind:src="`${API_URL}/${product.image[0].url}`"
-									class="object-contain w-full p-3 product-img h-2/3"
-									alt="${element.name}"
-								/>
-								<div
-									class="flex flex-col justify-around w-full p-4 h-1/3"
-								>
-									<h4 class="text-xl">{{ product.name }}</h4>
-									<p class="text-lg">
-										COP$ ${{ product.price }}
-									</p>
-									<div>
-										<div class="flex items-center text-md">
+<div class="bg-white">
+    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-full lg:px-8">
+      <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
+      <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div
+			v-for="(product, index) in products"
+			v-bind:key="index"
+			class="group relative">
+          <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+            <img :src="`${API_URL}/${product.image[0].url}`" :alt="product.name" class="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-gray-700">
+				<router-link
+					class="absolute inset-0"
+					v-bind:to="`/product/${product.id}`">
+
+				</router-link>
+				{{ product.name }}
+
+                <!-- <a v-bind:to="`/product/${product.id}`">
+                  <span aria-hidden="true" class="absolute inset-0" />
+                </a> -->
+              </h3>
+              <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+			  <div class="flex items-center text-md">
 											<div v-if="product.stock > 1">
 												<span
-													class="inline-block w-2 h-2 mr-2 bg-green-500 rounded-full"
-												></span>
-												<span class="text-green-500"
-													>Disponible</span
-												>
+													class="inline-block w-2 h-2 mr-2 bg-green-500 rounded-full"></span>
+												<span class="text-sm text-green-500">Disponible</span>
 											</div>
 
 											<div v-else>
 												<span
-													class="inline-block w-2 h-2 mr-2 bg-red-500 rounded-full"
-												></span>
-												<span class="text-red-500"
-													>No disponible</span
-												>
+													class="inline-block w-2 h-2 mr-2 bg-red-500 rounded-full"></span>
+												<span class="text-sm text-red-500">No disponible</span>
 											</div>
 										</div>
-									</div>
-								</div>
-							</router-link>
-						</li>
-					</ul>
-
-					<div class="barra"></div>
-
-					<div id="pagination" class="mt-4 mb-4">
+            </div>
+            <p class="text-sm font-medium text-gray-900">COP ${{ product.price }} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+	<div id="pagination" class="mt-4 flex items-center justify-center mb-4">
 						<button
 							v-for="(link, index) in links"
 							v-bind:key="index"
 							v-on:click="changePage(link.url)"
 							v-html="link.label"
 							v-bind:class="{ 'bg-gray-300': link.active }"
-							class="px-3 py-2 border border-slate-300 hover:bg-slate-300"
+							class="px-3 py-2 border  border-slate-300 hover:bg-slate-300"
 						></button>
 					</div>
-				</div>
+	</div>
 			</div>
 		</section>
 	</article>
